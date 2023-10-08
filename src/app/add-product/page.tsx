891a900ccaa09b9339e8d9bb7357f0dbac7fd9be
BeastNeedsMoreTorque <theironhammer1964@gmail.com>
2023-10-07 22:38:14 -0700

    added serve component stuff

--- src/app/add-product/page.tsx
+++ src/app/add-product/page.tsx
@@ -1,13 +1,32 @@
+import { prisma } from "@/lib/db/prisma";
+
 // import { metadata } from '../layout';
 export const metadata = {
   title: "Add Product",
 };
 
+async function addProduct(formData: formData) {
+  "use server";
+
+  const name = formData.get("name")?.toString();
+  const description = formData.get("description")?.toString();
+  const imageUrl = formData.get("imageUrl")?.toString();
+  const price = Number(formData.get("price") || 0);
+
+  if (!name || !description || !imageUrl || !price) {
+    throw Error("Missing required fields");
+  }
+
+  await prisma.product.create({
+    data: { name, description, imageUrl, price },
+  });
+}
+
 export default function AddProductPage() {
   return (
     <section>
       <h1 className="mb-3 text-lg font-bold">Add Product</h1>
-      <form>
+      <form action={addProduct}>
         <input
           required
           name="name"
